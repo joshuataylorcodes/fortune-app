e147991102230e79ff48f10b411bc11388642778
joshua <joshuamtaylor455@gmail.com>
2023-02-27 15:32:27 -0500

    lotto exercises

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -6,7 +6,9 @@ class MyExamplesController < ApplicationController
   def feature1_method
     array = ["you will get a dollar tomorrow", "you will get 0 dollars tomorrow", "you will get two dollars tomorrow"]
     x = rand(0..2)
-    render json: { message: array[x] }
+    #render json: { message: array[x] }
+
+    render json: { fortune: array.sample }
   end
 
   def rand_method
@@ -15,7 +17,7 @@ class MyExamplesController < ApplicationController
     #   y = rand(1..60).to_i
     #   numbers << y
     # end
-    # render json: { Lotto: numbers }
+    # render json: { lotto: numbers }
 
     numbers = []
     index = 0
@@ -25,5 +27,30 @@ class MyExamplesController < ApplicationController
       index = index += 1
     end
     render json: { lotto: numbers }
+  end
+
+  def ninenine_bottles
+    array = []
+    index = 0
+    x = 99
+    y = 98
+    n = "no"
+    while index < 99
+      lyric_line = "#{x} bottles of beer on the wall, #{x} bottles of beer,
+      Take one down and pass it around, #{y} bottles of beer on the wall."
+      array << lyric_line
+      x = x - 1
+      y = y - 1
+      index = index + 1
+    end
+    last_lyrics = "#{x} bottles of beer on the wall, #{x} bottles of beer,
+    Take one down and pass it around, #{n} bottles of beer on the wall."
+    array << last_lyrics
+    x = 99
+    last_lyrics = "#{n} bottles of beer on the wall, #{n} bottles of beer,
+    Take one down and pass it around, #{x} bottles of beer on the wall."
+    array << last_lyrics
+
+    render json: { lyrics: array }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  #get "fortune", controller: "my_examples", action: "feature1_method"
+  get "/fortune", controller: "my_examples", action: "feature1_method"
 
-  get "fortune", controller: "my_examples", action: "rand_method"
+  get "/lotto", controller: "my_examples", action: "rand_method"
+
+  get "/99bottles", controller: "my_examples", action: "ninenine_bottles"
 end
